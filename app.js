c0ba120ec89bd38bfdd20e166155a0b1d82f500d
Dean Holbrook <deanholbrook@MacBook-Pro.local>
2020-07-12 13:10:19 -0700

    changed context type to HTML

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Greetings, earthling!");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end(`<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8" />        
+            <title>Hello</title>
+        </head>
+        <body>
+            <h3>Greetings, earthling!</h3>
+        </body>
+    </html>
+    `);
 });
 
 const port = process.env.PORT || 1337;
